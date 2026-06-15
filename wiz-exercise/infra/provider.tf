5fc7bcc9ffbf15c9c6edc100a59ac77c35248085
Oleksii Androsov <oleksiiandrosov85@gmail.com>
2026-06-15 12:59:56 +0200

    Add S3 remote state backend and terraform apply with manual approval

--- wiz-exercise/infra/provider.tf
+++ wiz-exercise/infra/provider.tf
@@ -6,6 +6,12 @@ terraform {
     }
   }
   required_version = ">= 1.3.0"
+
+  backend "s3" {
+    bucket = "movie-buddy-tfstate-329153220664"
+    key    = "wiz-exercise/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
