1b06a4be9b6f3fafc569594ebceceb24fb3cfd8e
Oleksii Androsov <oleksiiandrosov85@gmail.com>
2026-06-16 15:08:08 +0200

    Restrict MongoDB access to K8s private subnets only

--- wiz-exercise/infra/ec2.tf
+++ wiz-exercise/infra/ec2.tf
@@ -46,11 +46,11 @@ resource "aws_security_group" "ec2_sg" {
   }
 
   ingress {
-    description = "MongoDB from VPC only"
+    description = "MongoDB from Kubernetes private subnets only"
     from_port   = 27017
     to_port     = 27017
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["10.0.2.0/24", "10.0.3.0/24"]
   }
 
   egress {
