e74ad839b43d69def321edcf61ecf372ea15917b
Oleksii Androsov <oleksiiandrosov85@gmail.com>
2026-06-20 15:33:13 +0200

    Simplify Config rules - remove recorder (already exists in account)

--- wiz-exercise/infra/config.tf
+++ wiz-exercise/infra/config.tf
@@ -1,67 +1,7 @@
-resource "aws_config_configuration_recorder" "main" {
-  name     = "${var.project_name}-recorder"
-  role_arn = aws_iam_role.config_role.arn
-
-  recording_group {
-    all_supported = true
-  }
-}
-
-resource "aws_config_configuration_recorder_status" "main" {
-  name       = aws_config_configuration_recorder.main.name
-  is_enabled = true
-  depends_on = [aws_config_delivery_channel.main]
-}
-
-resource "aws_s3_bucket" "config" {
-  bucket        = "${var.project_name}-config-${data.aws_caller_identity.current.account_id}"
-  force_destroy = true
-}
-
-resource "aws_config_delivery_channel" "main" {
-  name           = "${var.project_name}-delivery"
-  s3_bucket_name = aws_s3_bucket.config.bucket
-  depends_on     = [aws_config_configuration_recorder.main]
-}
-
-resource "aws_iam_role" "config_role" {
-  name = "${var.project_name}-config-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = { Service = "config.amazonaws.com" }
-      Action    = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "config_role" {
-  role       = aws_iam_role.config_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWS_ConfigRole"
-}
-
-resource "aws_iam_role_policy" "config_s3" {
-  name = "${var.project_name}-config-s3"
-  role = aws_iam_role.config_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect   = "Allow"
-      Action   = ["s3:PutObject", "s3:GetBucketAcl"]
-      Resource = [
-        aws_s3_bucket.config.arn,
-        "${aws_s3_bucket.config.arn}/*"
-      ]
-    }]
-  })
-}
-
 data "aws_caller_identity" "current" {}
 
 # Rule 1: SSH must not be open to the internet
+# Will immediately flag movie-buddy-ec2-sg as NON_COMPLIANT (intentional weakness)
 resource "aws_config_config_rule" "restricted_ssh" {
   name = "${var.project_name}-restricted-ssh"
 
@@ -69,11 +9,10 @@ resource "aws_config_config_rule" "restricted_ssh" {
     owner             = "AWS"
     source_identifier = "INCOMING_SSH_DISABLED"
   }
-
-  depends_on = [aws_config_configuration_recorder_status.main]
 }
 
 # Rule 2: S3 buckets must not allow public read access
+# Will immediately flag movie-buddy-tfstate bucket as NON_COMPLIANT (intentional weakness)
 resource "aws_config_config_rule" "s3_public_read_prohibited" {
   name = "${var.project_name}-s3-public-read-prohibited"
 
@@ -81,6 +20,4 @@ resource "aws_config_config_rule" "s3_public_read_prohibited" {
     owner             = "AWS"
     source_identifier = "S3_BUCKET_PUBLIC_READ_PROHIBITED"
   }
-
-  depends_on = [aws_config_configuration_recorder_status.main]
 }
